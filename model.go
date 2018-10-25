3cfb26935f31b8323dd05e8619f22ba2dfce34fc
salihkem <msalih@la.com.tr>
2018-10-25 10:48:20 +0300

    update

---
+++ model.go
@@ -0,0 +1,12 @@
+package main
+
+import "gopkg.in/mgo.v2/bson"
+
+type Item struct {
+	ID          bson.ObjectId `bson:"_id" json:"id" `
+	Name        *string       `bson:"name" json:"name"`
+	Value       *string       `bson:"value" json:"value"`
+	Description string        `bson:"description" json:"description"`
+	ItemId      string        `bson:"-" json:"-"`
+	Count       int           `bson:"count" json:"count"`
+}
