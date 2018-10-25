7df13896f737834aff56d23d9ba5c686df5871b9
salihkem <msalih@la.com.tr>
2018-10-25 14:19:48 +0300

    first git repo test for golang

--- mdata.go
+++ mdata.go
@@ -1,6 +1,17 @@
 package mdata
 
+import "gopkg.in/mgo.v2/bson"
+
 //Fahrenheit to Celsius
-func GetMe() string {
-	return "(f-32)/1.8"
+func TestMe() string {
+	return "Github repo testing is succeed"
+}
+
+type Item struct {
+	ID          bson.ObjectId `bson:"_id" json:"id" `
+	Name        *string       `bson:"name" json:"name"`
+	Value       *string       `bson:"value" json:"value"`
+	Description string        `bson:"description" json:"description"`
+	ItemId      string        `bson:"-" json:"-"`
+	Count       int           `bson:"count" json:"count"`
 }
