01b84f8fe136f5dcc207aaff1ce233bef5e048ab
salihkem <msalih@la.com.tr>
2018-10-25 13:31:40 +0300

    ddd

--- model.go
+++ model.go
@@ -2,18 +2,7 @@ package model
 
 import (
 	"fmt"
-
-	"gopkg.in/mgo.v2/bson"
 )
-
-type Item struct {
-	ID          bson.ObjectId `bson:"_id" json:"id" `
-	Name        *string       `bson:"name" json:"name"`
-	Value       *string       `bson:"value" json:"value"`
-	Description string        `bson:"description" json:"description"`
-	ItemId      string        `bson:"-" json:"-"`
-	Count       int           `bson:"count" json:"count"`
-}
 
 func testMyData() {
 	fmt.Print("working")
